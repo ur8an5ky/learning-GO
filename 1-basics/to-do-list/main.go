e0007c43586e0df77d346dc52f928e1595bdc3c8
ur8an5ky <urbanski.jakub77@gmail.com>
2026-05-23 23:10:28 +0200

    feat(to-do-list): add in-memory to-do list CLI

--- 1-basics/to-do-list/main.go
+++ 1-basics/to-do-list/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -26,20 +27,55 @@ type TaskList struct {
 	nextID int
 }
 
-func (tl *TaskList) Add(task string) {
+func (tl *TaskList) Add(task string) Task {
 	t := NewTask(tl.nextID, task)
 	tl.Tasks = append(tl.Tasks, t)
 
 	tl.nextID++
-
-	fmt.Printf("Added task #%d: %s\n", t.ID, task)
+	return t
 }
 
 func (tl *TaskList) List() {
-	fmt.Println("List of tasks:")
-	for _, t := range tl.Tasks {
-		fmt.Printf("\t<%d>: %s\n", t.ID, t.Text)
+	if len(tl.Tasks) == 0 {
+		fmt.Println("(no tasks)")
+		return
 	}
+	fmt.Println("List of tasks:")
+
+	for _, t := range tl.Tasks {
+		isDone := " "
+		if t.Done {
+			isDone = "X"
+		}
+		fmt.Printf("\t[%s]  #%d: %s\n", isDone, t.ID, t.Text)
+	}
+}
+
+func (tl *TaskList) Done(id int) error {
+	for i := range tl.Tasks {
+		if tl.Tasks[i].ID == id {
+			tl.Tasks[i].Done = true
+			return nil
+		}
+	}
+
+	return fmt.Errorf("task #%d not found", id)
+}
+
+func (tl *TaskList) Delete(id int) error {
+	it := -1
+	for i := range tl.Tasks {
+		if tl.Tasks[i].ID == id {
+			it = i
+			break
+		}
+	}
+	if it == -1 {
+		return fmt.Errorf("task #%d not found", id)
+	}
+
+	tl.Tasks = append(tl.Tasks[:it], tl.Tasks[it+1:]...)
+	return nil
 }
 
 func help() {
@@ -60,7 +96,7 @@ func main() {
 	tl := TaskList{nextID: 1}
 
 	for {
-		fmt.Print(">  ")
+		fmt.Print("> ")
 
 		if !scanner.Scan() {
 			fmt.Println("\nSee you later!")
@@ -75,18 +111,6 @@ func main() {
 		command := strings.SplitN(line, " ", 2)
 		command[0] = strings.ToLower(command[0])
 
-		//if command[0] == "add" {
-		//	if len(command) < 2 {
-		//		fmt.Println("Command:", command, "takes at least two arguments! Try again!")
-		//		continue
-		//	}
-		//} else {
-		//	if len(command) >= 2 {
-		//		fmt.Println("Command:", command, "takes only one argument! Try again!")
-		//		continue
-		//	}
-		//}
-
 		switch command[0] {
 		case "quit", "exit":
 			fmt.Println("See you later!")
@@ -95,12 +119,43 @@ func main() {
 			help()
 		case "add":
 			if len(command) < 2 {
-				fmt.Println("Command:", command, "takes at least two arguments! Try again!")
+				fmt.Println("Usage: add <text>")
 				continue
 			}
-			tl.Add(command[1])
+			task := tl.Add(command[1])
+			fmt.Printf("Added task #%d: %s\n", task.ID, task.Text)
 		case "list":
 			tl.List()
+		case "done":
+			if len(command) != 2 {
+				fmt.Println("Usage: done <id>.")
+				continue
+			}
+			id, err := strconv.Atoi(command[1])
+			if err != nil {
+				fmt.Println("ID must be a number.")
+				continue
+			}
+			if err := tl.Done(id); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Task #%d marked as done\n", id)
+		case "delete":
+			if len(command) != 2 {
+				fmt.Println("Usage: delete <id>.")
+				continue
+			}
+			id, err := strconv.Atoi(command[1])
+			if err != nil {
+				fmt.Println("ID must be a number!")
+				continue
+			}
+			if err := tl.Delete(id); err != nil {
+				fmt.Println(err)
+				continue
+			}
+			fmt.Printf("Task #%d deleted\n", id)
 		default:
 			fmt.Printf("Unknown command: %q. Type 'help' for available commands.\n", command[0])
 		}
