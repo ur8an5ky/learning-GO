5dfad569f64a96c3d7bc39c72023ade5a8f1b841
ur8an5ky <urbanski.jakub77@gmail.com>
2026-05-23 11:38:31 +0200

    feat(to-do-list): add structures for representation of Task and list of Tasks

--- 1-basics/to-do-list/main.go
+++ 1-basics/to-do-list/main.go
@@ -13,32 +13,51 @@ type Task struct {
 	Done bool
 }
 
+func NewTask(id int, text string) Task {
+	return Task{
+		ID:   id,
+		Text: text,
+		Done: false,
+	}
+}
+
 type TaskList struct {
-	Tasks []Task
+	Tasks  []Task
+	nextID int
+}
+
+func (tl *TaskList) Add(task string) {
+	t := NewTask(tl.nextID, task)
+	tl.Tasks = append(tl.Tasks, t)
+
+	tl.nextID++
+
+	fmt.Printf("Added task #%d: %s\n", t.ID, task)
+}
+
+func (tl *TaskList) List() {
+	fmt.Println("List of tasks:")
+	for _, t := range tl.Tasks {
+		fmt.Printf("\t<%d>: %s\n", t.ID, t.Text)
+	}
 }
 
 func help() {
 	fmt.Println("Available commands:")
-	fmt.Println("  add <text>   - add a new task")
-	fmt.Println("  list         - show all tasks")
-	fmt.Println("  done <id>    - mark task as done")
-	fmt.Println("  delete <id>  - delete a task")
-	fmt.Println("  help         - show this help")
-	fmt.Println("  quit, exit   - exit the program")
-}
-
-func add(task string) {
-	fmt.Println("Adding task:", task)
-}
-
-func list() {
-	fmt.Println("Listing tasks:")
+	fmt.Println("\tadd <text>   - add a new task")
+	fmt.Println("\tlist         - show all tasks")
+	fmt.Println("\tdone <id>    - mark task as done")
+	fmt.Println("\tdelete <id>  - delete a task")
+	fmt.Println("\thelp         - show this help")
+	fmt.Println("\tquit, exit   - exit the program")
+	fmt.Println()
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 
 	fmt.Println("Welcome to to-do! Type 'help' to see the available commands.")
+	tl := TaskList{nextID: 1}
 
 	for {
 		fmt.Print(">  ")
@@ -54,10 +73,19 @@ func main() {
 		}
 
 		command := strings.SplitN(line, " ", 2)
-		if (len(command) >= 2) && (command[0] != "add") {
-			fmt.Println("Command:", command, "takes only one argument! Try again!")
-			continue
-		}
+		command[0] = strings.ToLower(command[0])
+
+		//if command[0] == "add" {
+		//	if len(command) < 2 {
+		//		fmt.Println("Command:", command, "takes at least two arguments! Try again!")
+		//		continue
+		//	}
+		//} else {
+		//	if len(command) >= 2 {
+		//		fmt.Println("Command:", command, "takes only one argument! Try again!")
+		//		continue
+		//	}
+		//}
 
 		switch command[0] {
 		case "quit", "exit":
@@ -66,9 +94,13 @@ func main() {
 		case "help":
 			help()
 		case "add":
-			add(command[1])
+			if len(command) < 2 {
+				fmt.Println("Command:", command, "takes at least two arguments! Try again!")
+				continue
+			}
+			tl.Add(command[1])
 		case "list":
-			list()
+			tl.List()
 		default:
 			fmt.Printf("Unknown command: %q. Type 'help' for available commands.\n", command[0])
 		}
